68654f420d1bfed6ec5bab28c4d730bedfbc979c
Tallis92 <147599891+Tallis92@users.noreply.github.com>
2023-10-11 11:42:17 +0200

    First commit
    Changed "hello world" to "Hejsan världen".

---
+++ GitHub_Demo/GitHub_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHub_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hejsan världen!");
+        }
+    }
+}
